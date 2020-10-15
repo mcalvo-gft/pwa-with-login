f03a08869ccc19d51a497b0098f566ce9114f359
Miguel Calvo <miguel.calvo@gft.com>
2020-10-15 13:13:25 +0200

    fix: remove type conflict

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import * as firebase from 'services/firebase.service';
 
 export const login = (email: string, password: string): Promise<void | Error> => {
-  return firebase.login(email, password);
+  return firebase.login(email, password).then(() => Promise.resolve());
 };
 
 export const logout = (): void => {
